f3c75833eeba3ebcaa1b8d2f3e242a266db21058
Utkarsh Sharma <82648036+usharma-petroit@users.noreply.github.com>
2021-06-28 11:11:55 +0530

    Update HelloWorldController.java

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController 
 {
-@RequestMapping("/")
+@RequestMapping("/hello")
 public String hello() 
 {
 return "Hello javaTpoint";
